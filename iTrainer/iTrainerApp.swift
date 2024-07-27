29c2a936656705680e9b3c28c23d94be7cfd9aac
Andrey Kulinskiy <a.kulinskiy@newlinetechnologies.net>
2024-07-27 12:43:57 +0300

    Initial Commit

---
+++ iTrainer/iTrainerApp.swift
@@ -0,0 +1,32 @@
+//
+//  iTrainerApp.swift
+//  iTrainer
+//
+//  Created by Andrey Kulinskiy on 27.07.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct iTrainerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
