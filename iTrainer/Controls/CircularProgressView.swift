b8d15df0b69148d8a3adfd00fee8046b705a1306
Andrey Kulinskiy <a.kulinskiy@newlinetechnologies.net>
2024-08-21 20:15:09 +0300

    added CircularProgressView

---
+++ iTrainer/Controls/CircularProgressView.swift
@@ -0,0 +1,41 @@
+//
+//  CircularProgressView.swift
+//  iTrainer
+//
+//  Created by Andrey Kulinskiy on 21.08.2024.
+//
+
+import SwiftUI
+
+struct CircularProgressView: View {
+    
+    var lineWidth: CGFloat = 8
+    
+    var progress: Double = 0.75
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke( // 1
+                    Color.gray.opacity(0.5),
+                    lineWidth: lineWidth
+                )
+            Circle() // 2
+                .trim(from: 0.0, to: progress)
+                .stroke(
+                    Color.gray,
+                    style: StrokeStyle(
+                        lineWidth: lineWidth,
+                        lineCap: .round // round
+                    )
+                )
+                .rotationEffect(.degrees(-90))
+                .animation(.easeOut, value: progress)
+        }
+    }
+}
+
+#Preview {
+    CircularProgressView()
+        .frame(width: 50, height: 50)
+}
---
+++ iTrainer/Extensions/TimeInterval+Utils.swift
@@ -0,0 +1,8 @@
+//
+//  TimeInterval+Utils.swift
+//  iTrainer
+//
+//  Created by Andrey Kulinskiy on 21.08.2024.
+//
+
+import Foundation
